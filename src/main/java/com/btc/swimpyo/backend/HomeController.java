f61f026e3ec77e268617fbabddb933b69f6e3532
jinbeom <jinbeom3238@gmail.com>
2023-10-19 14:57:45 +0900

    git init

---
+++ src/main/java/com/btc/swimpyo/backend/HomeController.java
@@ -0,0 +1,19 @@
+package com.btc.swimpyo.backend;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Log4j2
+@RestController
+@RequestMapping("/api")
+public class HomeController {
+
+    @GetMapping("/hello")
+    public String home() {
+        log.info("hello");
+        return "This is Back Data";
+    }
+
+}
---
+++ src/test/java/com/btc/swimpyo/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.btc.swimpyo.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
