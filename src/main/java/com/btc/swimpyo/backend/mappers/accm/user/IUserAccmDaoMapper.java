f690e55e8dffa49077f4236f851d27cec5ad3511
iieunji023 <iieunji023@gmail.com>
2023-11-02 22:28:03 +0900

    사용자 숙박시설 리스트 조회 및 상세페이지 조회 기능 구현

--- src/main/java/com/btc/swimpyo/backend/mappers/accm/user/IUserAccmDaoMapper.java
+++ src/main/java/com/btc/swimpyo/backend/mappers/accm/user/IUserAccmDaoMapper.java
@@ -7,9 +7,20 @@ import java.util.List;
 
 @Mapper
 public interface IUserAccmDaoMapper {
-    
-    // 리스트 조회
-    public List<AdminAccmDto> selectAccmList(AdminAccmDto adminAccmDto);
 
+    /*
+     * 리스트 조회
+     */
+    // 이미지 제외한 정보
+    public List<AdminAccmDto> selectAccmList(AdminAccmDto adminAccmDto);
+    // a_acc_no 값 조회
+    public List<Integer> selectAccmNo(AdminAccmDto adminAccmDto);
+    // 이미지 조회
+    public List<String> selectAccmImgList(int a_acc_no);
+
+    // 상세페이지 조회
     public AdminAccmDto selectAccmDetail(int a_acc_no);
+
+
+
 }
--- src/main/java/com/btc/swimpyo/backend/service/accm/user/UserAccmService.java
+++ src/main/java/com/btc/swimpyo/backend/service/accm/user/UserAccmService.java
@@ -5,7 +5,11 @@ import com.btc.swimpyo.backend.mappers.accm.user.IUserAccmDaoMapper;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Log4j2
 @Service
@@ -19,27 +23,62 @@ public class UserAccmService implements IUserAccmService{
 
     // 리스트 조회
     @Override
-    public List<AdminAccmDto> showAccmList(AdminAccmDto adminAccmDto) {
+    public Map<String, Object> showAccmList(AdminAccmDto adminAccmDto) {
         log.info("[UserAccmService] showAccmList()");
 
-        List<AdminAccmDto> adminAccmDtos = iUserAccmDaoMapper.selectAccmList(adminAccmDto);
+        int a_acc_no = adminAccmDto.getA_acc_no();
+        Map<String,Object> msgData = new HashMap<>();
 
+        List<AdminAccmDto> adminAccmDtos;
+
+        adminAccmDtos = iUserAccmDaoMapper.selectAccmList(adminAccmDto);
         log.info("[UserAccmService] adminAccmDtos: " + adminAccmDtos);
 
-        return adminAccmDtos;
+        // a_acc_no 값 가져오기
+        List<Integer> a_acc_nos = iUserAccmDaoMapper.selectAccmNo(adminAccmDto);
+
+        for(int i = 0; i<a_acc_nos.size(); i++) {
+            a_acc_no = a_acc_nos.get(i);
+            log.info("a_acc_no: " + a_acc_no);
+
+        }
+
+        // a_acc_no로 이미지 가져오기
+        List<String> adminImgDtos = iUserAccmDaoMapper.selectAccmImgList(a_acc_no);
+
+        log.info("[UserAccmService] adminImgDtos: " + adminImgDtos);
+
+        msgData.put("adminAccmDtos", adminAccmDtos);
+        msgData.put("adminImgDtos", adminImgDtos);
+
+        log.info("[UserAccmService] msgData: " + msgData);
+
+
+        return msgData;
 
     }
 
     // 상세페이지 보기
     @Override
-    public AdminAccmDto showAccmDetail(int a_acc_no) {
+    public Map<String, Object> showAccmDetail(int a_acc_no) {
         log.info("[UserAccmService] showAccmDetail()");
 
-        AdminAccmDto adminAccmDtos = iUserAccmDaoMapper.selectAccmDetail(a_acc_no);
+        Map<String, Object> msgData = new HashMap<>();
 
-        log.info("[UserAccmService] adminAccmDtos: " + adminAccmDtos);
+        AdminAccmDto adminAccmDto = iUserAccmDaoMapper.selectAccmDetail(a_acc_no);
+        log.info("[UserAccmService] adminAccmDtos: " + adminAccmDto);
+        log.info("[UserAccmService] a_acc_no: " + a_acc_no);
 
-        return adminAccmDtos;
+        // 이미지 정보 가져오기
+        List<String> a_i_images = iUserAccmDaoMapper.selectAccmImgList(a_acc_no);
+        log.info("[UserAccmService] a_i_images: " + a_i_images);
+
+        msgData.put("adminAccmDto", adminAccmDto);
+        msgData.put("a_i_images", a_i_images);
+        log.info("[UserAccmService] msgData: " + msgData);
+
+
+        return msgData;
 
     }
 
